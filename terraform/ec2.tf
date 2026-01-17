b95d3a48f6c5e3a530fa1333738d2b24fd9fd5d0
frdamirul <frdamirul09@gmail.com>
2026-01-17 16:09:58 +0800

    add new keypair.tf file and amend ansible block in ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -17,7 +17,6 @@ resource "aws_instance" "ansible" {
   subnet_id              = aws_subnet.private.id
   private_ip             = "10.0.0.135"
   vpc_security_group_ids = [aws_security_group.private_sg.id]
-  iam_instance_profile   = aws_iam_instance_profile.profile.name
 
   tags = {
     Name = "ansible-controller"
@@ -26,28 +25,14 @@ resource "aws_instance" "ansible" {
    user_data = <<-EOF
              #!/bin/bash
               # --- update & upgrade OS
-              apt update && apt upgrade -y
+              sudo apt update && apt upgrade -y
 
               # --- install pipx jika belum ada
-              apt install -y pipx
+              sudo apt install -y pipx
 
               # --- install ansible melalui pipx
               pipx install --include-deps ansible
               pipx ensurepath
-
-              # --- install dependencies untuk SSM
-              apt install -y unzip curl python3-pip
-              pip3 install boto3 botocore amazon.aws
-
-              # --- install AWS SSM plugin
-              curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o ssm.deb
-              dpkg -i ssm.deb
-
-              # --- buat folder structure Ansible
-              mkdir -p /home/ubuntu/ansible/{inventory,playbooks,roles/docker/tasks,roles/web/tasks,roles/monitoring/tasks}
-
-              # --- set ownership
-              chown -R ubuntu:ubuntu /home/ubuntu/ansible
 
               EOF
 }
