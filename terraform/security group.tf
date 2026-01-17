03c1eb2f459047af46a1c7eef17fcf438bda9e27
frdamirul <frdamirul09@gmail.com>
2026-01-17 17:33:35 +0800

    change destination IP in private SG block

--- terraform/security group.tf
+++ terraform/security group.tf
@@ -32,7 +32,7 @@ resource "aws_security_group" "private_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/24"]
   }
 
   egress {
