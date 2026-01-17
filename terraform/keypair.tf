e6e555445f60921b3f971c075a01df91a155e670
frdamirul <frdamirul09@gmail.com>
2026-01-17 16:37:45 +0800

    add keypair exception in .gitignore file && add logic for keypair

--- terraform/keypair.tf
+++ terraform/keypair.tf
@@ -1,14 +1,21 @@
 # Generate a new SSH key pair if one doesn't exist
+# Generate local SSH key (if you don't have one)
 resource "tls_private_key" "ansible_key" {
   algorithm = "RSA"
   rsa_bits  = 2048
 }
 
+# Use existing AWS key pair if exists, or create a new one
 resource "aws_key_pair" "ansible_key" {
   key_name   = "ansible-key"
   public_key = tls_private_key.ansible_key.public_key_openssh
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
+# Save private key locally for SSH access
 resource "local_file" "ansible_private_key" {
   content  = tls_private_key.ansible_key.private_key_pem
   filename = "${path.module}/ansible_key.pem"
