1952c6360e2ec646d65601cdff5457de54bef034
frdamirul <frdamirul09@gmail.com>
2026-01-17 17:06:54 +0800

    enhance the code

--- terraform/keypair.tf
+++ terraform/keypair.tf
@@ -1,11 +1,14 @@
-# Generate a new SSH key pair if one doesn't existfit
-# Generate local SSH key (if you don't have one)
+# ----------------------------
+# Generate a local SSH key for Ansible
+# ----------------------------
 resource "tls_private_key" "ansible_key" {
   algorithm = "RSA"
   rsa_bits  = 2048
 }
 
-# Use existing AWS key pair if exists, or create a new one
+# ----------------------------
+# Create AWS Key Pair using the public key
+# ----------------------------
 resource "aws_key_pair" "ansible_key" {
   key_name   = "ansible-key"
   public_key = tls_private_key.ansible_key.public_key_openssh
@@ -15,9 +18,11 @@ resource "aws_key_pair" "ansible_key" {
   }
 }
 
-# Save private key locally for SSH access
+# ----------------------------
+# Save private key locally so we can SSH
+# ----------------------------
 resource "local_file" "ansible_private_key" {
-  content  = tls_private_key.ansible_key.private_key_pem
-  filename = "${path.module}/ansible_key.pem"
+  content         = tls_private_key.ansible_key.private_key_pem
+  filename        = "${path.module}/ansible_key.pem"  # local file path
   file_permission = "0600"
 }
